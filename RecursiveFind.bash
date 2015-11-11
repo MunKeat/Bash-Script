7247f1214ce76eb9982fe15aacabb14f44a0f9f0
MunKeat <a0129561@u.nus.edu>
2015-11-12 00:58:21 +0800

    Created reference RecursiveFind.bash
    N.B. RecursiveFind.bash is intended for reference only, not as a script to
    be executed.

---
+++ RecursiveFind.bash
@@ -0,0 +1,14 @@
+##########################################
+# SNIPPET: RecursiveFind.bash
+#
+# DATE: 11-NOV-2015
+#
+# PURPOSE: Allow searching recursively from
+#   a starting directory. For reference.
+##########################################
+
+# (1) Find all files in (sub)directories recursively
+find . -type f
+
+# (2) Specify file extension
+find . -type f -name "*.extension"
