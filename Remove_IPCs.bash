04d00bcf15db8eca480e5e9ced4b6ab5d59f8430
MunKeat <a0129561@u.nus.edu>
2015-11-12 01:30:18 +0800

    Added Remove_IPCs.bash
    1. Remove_IPCs.bash cleanup any semaphore(s) and/or shared memory
    region(s) generated by the user.

---
+++ Remove_IPCs.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+##########################################
+# SCRIPT: Remove_IPCs.bash
+#
+# AUTHOR: MunKeat
+#   (Adapted from: http://stackoverflow.com/questions/2143404/delete-all-shared-memory-and-semaphores-on-linux)
+#
+# DATE: 11-NOV-2015
+#
+# PURPOSE: Cleanup semaphores and ICPs generated
+##########################################
+
+##########################################
+# FILES AND VARIABLES DEFINITION(S)
+##########################################
+ME=$(whoami)
+
+# ipcs -s: Print info about active semaphore(s).
+IPCS_S=$(ipcs -s | grep "$ME" | awk '{ print $2 }')
+# ipcs -m: Print info about active shared memory segment(s).
+IPCS_M=$(ipcs -m | grep "$ME" | awk '{ print $2 }')
+# ipcs -q: Print info about active message queue(s).
+IPCS_Q=$(ipcs -q | grep "$ME" | awk '{ print $2 }')
+
+##########################################
+# MAIN BODY
+##########################################
+for id in $IPCS_M; do
+  ipcrm -m $id;
+done
+
+for id in $IPCS_S; do
+  ipcrm -s $id;
+done
+
+for id in $IPCS_Q; do
+  ipcrm -q $id;
+done
