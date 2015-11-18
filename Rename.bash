9b53250aa16aa3179f6b9cbf268377b46df7ba39
MunKeat <a0129561@u.nus.edu>
2015-11-19 03:07:40 +0800

    Rough Draft of Rename.bash
    1. Created functions to append & prepend strings to listed file(s)
    2. Created functions to remove & replace strings in listed file(s)

--- Rename.bash
+++ Rename.bash
@@ -11,6 +11,7 @@
 #   (1) Appending
 #   (2) Prepending
 #   (3) Replacing
+#   (3) Removing
 ##########################################
 
 ##########################################
@@ -31,16 +32,51 @@ append()
   fi
 
   # Confirmation
-  printf "%-25s %s\n" "Original" "Appended"
+  printf "%-25s %s\n" "Original" "Append"
   printf -- -----------------------------------------------"\n"
   for file in $FILES; do
     # Use printf to format output; %-10s will generate left-aligned; %10, right-aligned
+    printf "%-25s %-25s\n" "$file" "$file$1"
+  done
+  printf -- -----------------------------------------------"\n"
+
+  while : ; do
+    printf "Continue? (y/n) "
+    read confirmation
+
+    case "$confirmation" in
+      y | Y )
+          break;;
+      n | N )
+          return;;
+    esac
+  done
+
+  # Renaming
+  for file in $FILES; do
+    mv "$file"{,"$1"};
+  done
+}
+
+prepend()
+{
+  # Verify that only 1 parameter was entered
+  if [[ "$#" -ne 1 ]]; then
+    echo "Usage: prepend <string>"
+    echo "Hint: Use 'Quotation' to encase your string."
+    return
+  fi
+
+  # Confirmation
+  printf "%-25s %s\n" "Original" "Prepend"
+  printf -- -----------------------------------------------"\n"
+  for file in $FILES; do
     printf "%-25s %-25s\n" "$file" "$1$file"
   done
   printf -- -----------------------------------------------"\n"
 
   while : ; do
-    echo "Continue? (y/n)"
+    printf "Continue? (y/n) "
     read confirmation
 
     case "$confirmation" in
@@ -54,6 +90,84 @@ append()
   # Renaming
   for file in $FILES; do
     mv {,"$1"}"$file";
+  done
+}
+
+remove()
+{
+  # Verify that only 1 parameter was entered
+  if [[ "$#" -ne 1 ]]; then
+    echo "Usage: remove <string>"
+    echo "Hint: Use 'Quotation' to encase your string."
+    return
+  fi
+
+  # Confirmation
+  printf "%-25s %s\n" "Original" "Removed"
+  printf -- -----------------------------------------------"\n"
+  for file in $FILES; do
+    if [[ "$file" =~ .*"$1".* ]]; then
+      printf "%-25s %-25s\n" "$file" "${file/$1/}"
+    fi
+  done
+  printf -- -----------------------------------------------"\n"
+
+  while : ; do
+    printf "Continue? (y/n) "
+    read confirmation
+
+    case "$confirmation" in
+      y | Y )
+          break;;
+      n | N )
+          return;;
+    esac
+  done
+
+  # Renaming
+  for file in $FILES; do
+    if [[ "$file" =~ .*"$1".* ]]; then
+      mv "$file" "${file/$1/}"
+    fi
+  done
+}
+
+replace()
+{
+  # Verify that only 1 parameter was entered
+  if [[ "$#" -ne 2 ]]; then
+    echo "Usage: replace <old> <new>"
+    echo "Hint: Use 'Quotation' to encase your string."
+    return
+  fi
+
+  # Confirmation
+  printf "%-25s %s\n" "Original" "Replaced"
+  printf -- -----------------------------------------------"\n"
+  for file in $FILES; do
+    if [[ "$file" =~ .*"$1".* ]]; then
+      printf "%-25s %-25s\n" "$file" "${file/$1/$2}"
+    fi
+  done
+  printf -- -----------------------------------------------"\n"
+
+  while : ; do
+    printf "Continue? (y/n) "
+    read confirmation
+
+    case "$confirmation" in
+      y | Y )
+          break;;
+      n | N )
+          return;;
+    esac
+  done
+
+  # Renaming
+  for file in $FILES; do
+    if [[ "$file" =~ .*"$1".* ]]; then
+      mv "$file" "${file/$1/$2}"
+    fi
   done
 }
 
