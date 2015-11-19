77894fa4cb6d22ac8db2b2fb8354c3ad666fc435
MunKeat <a0129561@u.nus.edu>
2015-11-19 11:45:42 +0800

    Implementation of main in RandomFileGenerator.bash

--- RandomFileGenerator.bash
+++ RandomFileGenerator.bash
@@ -16,8 +16,11 @@
 ##########################################
 # FILES AND VARIABLES DEFINITION(S)
 ##########################################
-numArg="${#}"
 binaryFlag="false"
+fileName=""
+filesize=0
+numArg="${#}"
+numOfFiles=1
 
 ##########################################
 # FUNCTION DEFINITION(S)
@@ -79,6 +82,7 @@ while : ; do
   read -p "Enter command: " -a order
 done
 
+# Extract binary flag, if present
 if [[ ${order[0]} = "-b" ]]; then
   binaryFlag="true"
   # Shift
@@ -86,3 +90,30 @@ if [[ ${order[0]} = "-b" ]]; then
   order=( "${order[@]}" )
 fi
 
+# Extract file multiples
+if [[ "${order[2]}" ]] && [[ "${order[2]}" -eq "${order[2]}" ]]; then
+  numOfFiles="${order[2]}"
+fi
+
+# Extract remaining variables
+fileName="${order[0]}"
+filesize="${order[1]}"
+
+if [[ "$numOfFiles" -gt 1 ]]; then
+  for ((index = 0; index <= $numOfFiles ; index++)); do
+    # Format number; new variable used as 0N is intepreted as hexadecimal
+    indexing=$(printf %03d "$index")
+
+    if [[ "$binaryFlag" = "true" ]]; then
+      generate_binary "$fileName$indexing" "$filesize"
+    else
+      generate_alphanum "$fileName$indexing" "$filesize"
+    fi
+  done
+else
+  if [[ "$binaryFlag" = "true" ]]; then
+    generate_binary "$fileName" "$filesize"
+  else
+    generate_alphanum "$fileName" "$filesize"
+  fi
+fi
